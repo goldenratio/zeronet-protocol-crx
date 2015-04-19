54c27db35131cc69237b2df8b5e64ad223880a29
makubex <karthikeyanvj@gmail.com>
2015-04-20 01:35:35 +0300

    - if ZeroNet is running on remote machine, then 127.0.0.1:43110 will be proxy'd through it.

--- js/background.js
+++ js/background.js
@@ -5,21 +5,44 @@
 
 function onBeforeRequest(details)
 {
-		
 	var currentURLRequest = document.createElement('a');
 	currentURLRequest.href = details.url;
 
-	// check for "zero" hostname
-	var isZeroTLD = currentURLRequest.hostname.slice(-5) == ZERO_TLD;
-	var isZeroHost = currentURLRequest.hostname.toLowerCase() == ZERO_HOST;
+	//console.log("hostname " + currentURLRequest.hostname +", protocol " + currentURLRequest.protocol, "url " + currentURLRequest.href);
+
+	var isZeroTLD = false;
+	var isZeroHost = false;
+
+	// tld
+	var currentTLD = currentURLRequest.hostname.slice(-5);
+	for(var i = 0; i < ZERO_ACCEPTED_HOSTS.length; i++)
+	{
+		if(currentTLD == ZERO_ACCEPTED_TLDS[i])
+		{
+			isZeroTLD =  true;
+			break;
+		}
+	}
+
+	// host
+	var currentHost = currentURLRequest.host.toLowerCase();
+	for(i = 0; i < ZERO_ACCEPTED_HOSTS.length; i++)
+	{
+		if(currentHost == ZERO_ACCEPTED_HOSTS[i])
+		{
+			isZeroHost =  true;
+			break;
+		}
+	}
+
 
 	if(isZeroTLD == false && isZeroHost == false)
 	{
-		// not a zeronet TLD, return immediately
+		// not a zeroNet TLD or Host, return immediately
 		return;
 	}
 
-		
+
 	//console.log("on before request! ", details);	
 	//console.log("hostname " + currentURLRequest.hostname +", protocol " + currentURLRequest.protocol, "url " + currentURLRequest.href);
 	
@@ -36,45 +59,76 @@ function onBeforeRequest(details)
 
 function handleProxy(zeroHostData) 
 {
-	console.log("handle proxy " + zeroHostData);
-	zeroHostData = zeroHostData || DEFAULT_ZERO_HOSTDATA;
+	//console.log("handle proxy " + zeroHostData);
+	zeroHostData = zeroHostData || DEFAULT__ZERO_HOST_DATA;
 
-	var config = {
-		mode: "pac_script",
-		pacScript: {
-			data: "function FindProxyForURL(url, host) {\n" +
-			"  if (shExpMatch(host, '*.zero') || host == '" + ZERO_HOST + "')\n" +
-			"    return 'PROXY " + zeroHostData + "';\n" +
-			"  return 'DIRECT';\n" +
-			"}"
-		}		
+	var config = getPacConfig(zeroHostData);
 
-	};
 
 	chrome.proxy.settings.set(
 		{
-			value: config, 
+			value: config,
 			scope: "regular"
 		},
 		function() {
-			console.log("proxy set done!");
+			//console.log("proxy set done!");
 		}
 	);
 
 }
 
-var ZERO_TLD = ".zero";
-var ZERO_HOST = "zero";
-var DEFAULT_ZERO_HOSTDATA = "127.0.0.1:43110";
 
-//var filter = { urls: ["<all_urls>"] };
-var filter = { urls: ["*://*.zero/*", "*://zero/*"] };
+function getPacConfig(zeroHostData)
+{
+	//console.log("getPacConfig " + zeroHostData);
+	var pacConfigWithLocalHost = {
+		mode: "pac_script",
+		pacScript: {
+			data: "function FindProxyForURL(url, host) {\n" +
+			"  if (shExpMatch(host, '*.zero') || host == 'zero' || shExpMatch(url, '*127.0.0.1:43110*') || shExpMatch(url, '*localhost:43110*'))\n" +
+			"    return 'PROXY " + zeroHostData + "';\n" +
+			"  return 'DIRECT';\n" +
+			"}"
+		}
+
+	};
+
+	var pacConfig = {
+		mode: "pac_script",
+		pacScript: {
+			data: "function FindProxyForURL(url, host) {\n" +
+			"  if (shExpMatch(host, '*.zero') || host == 'zero')\n" +
+			"    return 'PROXY " + zeroHostData + "';\n" +
+			"  return 'DIRECT';\n" +
+			"}"
+		}
+
+	};
+
+	if(zeroHostData != DEFAULT__ZERO_HOST_DATA)
+	{
+		// user is running ZeroNet in remote machine
+		// we can proxy 127.0.0.1:43110 and localhost:43110 to go through remote machine
+		return pacConfigWithLocalHost;
+	}
+
+	// user is ZeroNet running in his/her current machine, so we can't proxy 127.0.0.1:43110 connections
+	// will end in infinite loop.
+	return pacConfig;
+}
+
+var ZERO_ACCEPTED_TLDS = [".zero"]; // if you modify this also change pacScript (sorry)
+var ZERO_ACCEPTED_HOSTS = ["zero", "127.0.0.1:43110", "localhost:43110"]; // if you modify this also change pacScript (sorry)
+
+var DEFAULT__ZERO_HOST_DATA = "127.0.0.1:43110";
+
+var filter = { urls: ["<all_urls>"] };
 var opt_extraInfoSpec = ["blocking"];
 
 chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, filter, opt_extraInfoSpec);
 
-// clear proxy
-//chrome.proxy.settings.clear("regular", function(){ console.log("proxy cleared!")});
+// to clear proxy
+// remove and add the extension again
 
 // proxy error listener
 chrome.proxy.onProxyError.addListener(function(details){
